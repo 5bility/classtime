dd0e958a1af9c170c5c90b0c284d50e6ba115bc0
rt3310 <btac3310@gmail.com>
2021-12-03 02:27:54 +0900

    기본 개발 환경 설정

---
+++ config.py
@@ -0,0 +1,7 @@
+# 프로젝트 환경 설정
+import os
+
+BASE_DIR = os.path.dirname(__file__)
+
+SQLALCHEMY_DATABASE_URI = 'mysql://classtimepm:ct1234@localhost/classtime?charset=utf8'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
